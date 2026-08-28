22cf93fbc3a1bfb5b796b0e6bf5e364f014edbca
abidbhat257-droid <abidbhat257@gmail.com>
2026-08-28 20:46:00 +0530

    Import Contact component in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Dashboard from "@/pages/Dashboard";
 import CalculatorPage from "@/pages/CalculatorPage";
+import Contact from "./pages/Contact";
 import Compare from "@/pages/Compare";
 
 function App() {
